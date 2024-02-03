b8834e74d4e67318719b6355b3486e9a37094442
bakug <bakugan_a2002@mail.ru>
2024-02-03 20:25:08 +0300

    Много всего сделано

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,5 @@
 import { Revenue } from './definitions';
+import {log} from "util";
 
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString('en-US', {
@@ -24,6 +25,7 @@ export const formatDateToLocal = (
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
+  // console.log(revenue)
   const yAxisLabels = [];
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
